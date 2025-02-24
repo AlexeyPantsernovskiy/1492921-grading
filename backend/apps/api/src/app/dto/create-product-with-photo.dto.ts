6fd60f534e829ada1e26ac9e7b1cbf884b0e9464
Алексей Панцерновский <pan_alexey@mail.ru>
2025-02-24 13:54:03 +0600

    При добавлении товара сделал загрузку фото обязательным

--- backend/apps/api/src/app/dto/create-product-with-photo.dto.ts
+++ backend/apps/api/src/app/dto/create-product-with-photo.dto.ts
@@ -1,10 +1,10 @@
-import { IsOptional } from 'class-validator';
 import { ApiProperty, OmitType } from '@nestjs/swagger';
 
 import { CreateProductDto, ShopProductProperty } from '@project/shop-product';
 
-export class CreateProductPhotoDto extends OmitType(CreateProductDto, ['photo']) {
+export class CreateProductPhotoDto extends OmitType(CreateProductDto, [
+  'photo',
+]) {
   @ApiProperty(ShopProductProperty.PhotoFile.Description)
-  @IsOptional()
-  public photoFile?: Express.Multer.File;
+  public photoFile: Express.Multer.File;
 }
--- backend/apps/api/src/app/dto/update-product-with-photo.dto.ts
+++ backend/apps/api/src/app/dto/update-product-with-photo.dto.ts
@@ -3,7 +3,9 @@ import { ApiProperty, OmitType } from '@nestjs/swagger';
 
 import { ShopProductProperty, UpdateProductDto } from '@project/shop-product';
 
-export class UpdateProductPhotoDto extends OmitType(UpdateProductDto, ['photo']) {
+export class UpdateProductPhotoDto extends OmitType(UpdateProductDto, [
+  'photo',
+]) {
   @ApiProperty(ShopProductProperty.PhotoFile.Description)
   @IsOptional()
   public photoFile?: Express.Multer.File;
